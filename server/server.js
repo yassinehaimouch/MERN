0e55f9898e196a8789deb03d150fca3a5464ae62
yassinehaimouch <haymouch53@gmail.com>
2024-02-26 16:55:56 +0100

    step-4-front-back

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 // Create the server
 const express = require("express");
 const app = express();
+// Import CORS
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 const port = 3002;
+// Import schema
+const Username = require('./models/Users')
 
 // Import Mongoose
 const mongoose = require("mongoose");
@@ -11,15 +17,58 @@ require('dotenv').config()
 
 // Import UserModel
 const UserModel = require('./models/Users');
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.send("Hello World!")
+})
+
 
 // Add GET Method to server
-app.get('/', async(req, res) => {
-  
+app.get('/users', async(req, res) => {
   // Get users from mongodb
   const users = await UserModel.find()
   // Send response from server to "/"
   res.json(users)
 })
+
+app.post('/createuser', (req, res) => {
+  const newUser = new Username(req.body);
+  newUser
+    .save()
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+
+// Route for Delete a book
+app.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Username.findByIdAndDelete(id);
+    if (!result) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    return res.status(200).send({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.put('/:id', async (req, res) => {
+  const user = await Username.findById(req.params.id);
+  user.name = req.body.name;
+  user.age = req.body.age;
+  user.email = req.body.email;
+  user.save();
+  res.json(user);
+})
+
 
 mongoose
   .connect(
